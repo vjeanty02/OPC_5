e04b02e6cda9a2d22517bafd1d42dd446f9ccdce
Virlus Jeanty <vjeanty02@gmail.com>
2023-02-14 04:47:37 -0500

    View multiple items by scrolling through lists

--- front/js/script.js
+++ front/js/script.js
@@ -4,16 +4,20 @@ const pieces = await reponse.json();
 
 
 // Création des balises 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.imageUrl;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.name;
-const prixElement = document.createElement("p");
-prixElement.innerText = article.price;
 
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".items");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
+for (let i = 0; i < pieces.length; i++) {
+
+    const article = pieces[i];
+    const imageElement = document.createElement("img");
+    imageElement.src = article.imageUrl;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.name;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = article.price;
+
+    //Rattachement de nos balises au DOM
+    const sectionFiches = document.querySelector(".items");
+    sectionFiches.appendChild(imageElement);
+    sectionFiches.appendChild(nomElement);
+    sectionFiches.appendChild(prixElement);
+}
