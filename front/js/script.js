c62d46a24c881162e8712af326195bcf7efe137f
Virlus Jeanty <vjeanty02@gmail.com>
2023-02-14 04:16:03 -0500

    Create new DOM elements

--- front/js/script.js
+++ front/js/script.js
@@ -2,3 +2,18 @@
 const reponse = await fetch('http://localhost:3000/api/products');
 const pieces = await reponse.json();
 
+
+// Création des balises 
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.imageUrl;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.name;
+const prixElement = document.createElement("p");
+prixElement.innerText = article.price;
+
+//Rattachement de nos balises au DOM
+const sectionFiches = document.querySelector(".items");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
