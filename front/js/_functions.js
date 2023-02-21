bb5978add6b53d4a8463fd54e034202726c76810
Virlus Jeanty <vjeanty02@gmail.com>
2023-02-20 23:20:22 -0500

    Create function.js

---
+++ front/js/_functions.js
@@ -0,0 +1,6 @@
+// Retrieve the products from the JSON file
+// Add html with javascript
+// Add html as text with javascript
+
+// Add the product to the basket
+// Remove the product from the basket
