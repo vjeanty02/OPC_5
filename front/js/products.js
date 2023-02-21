7ede1efca3a89c8d8eadab4637948b8244259d4e
Virlus Jeanty <vjeanty02@gmail.com>
2023-02-21 03:12:04 -0500

    Update script.js

--- front/js/products.js
+++ front/js/products.js
@@ -6,14 +6,14 @@ const reponse = await fetch('http://localhost:3000/api/products/'+id);
 const product = await reponse.json();
 
 
-document.getElementById("item__img").innerHTML = '<img src = "'+ product.imageUrl + '" alt="Photographie d\'un canapé"></img>'
-document.getElementById("title").innerHTML = product.name;
-document.getElementById("price").innerHTML = product.price;
-document.getElementById("description").innerHTML = product.description;
+document.querySelector("#item__img img").src = product.imageUrl;
+document.querySelector("#title").innerText = product.name;
+document.querySelector("#price").innerText = product.price;
+document.querySelector("#description").innerText = product.description;
 
 for (let i = 0; i < product.colors.length; i++) {
     const element = product.colors[i];
-    document.getElementById("colors").innerHTML += '<option value="'+element+'">'+element+'</option>';
+    document.querySelector("#colors").innerHTML += '<option value="'+element+'">'+element+'</option>';
 }
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,22 @@
-// Récupération des produits depuis le fichier JSON
-const reponse = await fetch('http://localhost:3000/api/products');
-const product = await reponse.json();
+// Get products from JSON file
+const response = await fetch('http://localhost:3000/api/products');
+const product = await response.json();
 
+// Add products to homepage (index.html)
 for (let i = 0; i < product.length; i++) {
-
     const article = product[i];
-    document.getElementById("items").innerHTML += '<a href="./product.html?id='+ article._id +'"><article><img src="'+article.imageUrl+'" alt="Lorem ipsum dolor sit amet, Kanap name1"><h3 class="productName">'+article.name+'</h3><p class="productDescription">'+article.description+'</p></article></a>'
+    document.querySelector('#items').innerHTML += `
+    <a class="product-${i}" href="./product.html?id=">
+        <article>
+            <img class="product-image-${i}" src="" alt="Lorem ipsum dolor sit amet, Kanap name1">
+            <h3 class="product-name-${i}" class="productName"></h3>
+            <p class="product-description-${i}"></p>
+        </article>
+    </a>`
+    document.querySelector(`.product-${i}`).href = `./product.html?id=${article._id}`;
+    document.querySelector(`.product-image-${i}`).src = article.imageUrl;
+    document.querySelector(`.product-name-${i}`).innerText = article.name;
+    document.querySelector(`.product-description-${i}`).innerText = article.description;
 }
 
 
